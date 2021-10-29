aeddf4f4364bd4c5bc1eef7160df794d6a6f4190
Jet <rondellivey03@gmail.com>
2021-10-29 09:44:45 -0400

    first commit

---
+++ vehicles.js
@@ -0,0 +1,673 @@
+var vehicles = [
+    {
+        id: 0,
+        name: "2011 Ford Focus RS",
+        type: "Compact",
+        price: 35999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 4,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: "2011 Ford Focus SVT",
+        type: "Compact",
+        price: 19999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 65,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 4,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "2005 Volkswagen Golf R32",
+        type: "Compact",
+        price: 45999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "2015 Volkswagen Scirocco R",
+        type: "Compact",
+        price: 19999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 0,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "1991 Honda CR-X SiR",
+        type: "Compact",
+        price: 19999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 60,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "2016 BMW 1 Series M",
+        type: "Compact",
+        price: 78999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 85,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: "1997 Honda Civic Type R",
+        type: "Compact",
+        price: 10899,
+        data: [
+            {
+                onclick: null,
+                top_speed: 65,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 7,
+        name: "1997 Volvo 850 R",
+        type: "Compact",
+        price: 10200,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 8,
+        name: "2014 Toyota Prius XW30",
+        type: "Compact",
+        price: 16999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 45,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: "2011 Nissan Leaf",
+        type: "Compact",
+        price: 0,
+        data: [
+            {
+                onclick: null,
+                top_speed: 0,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 10,
+        name: "1999 Mitsubishi Lancer GSR Evolution VI",
+        type: "Compact",
+        price: 99999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 11,
+        name: "1987 Shelby Omni GLHS",
+        type: "Compact",
+        price: 13599,
+        data: [
+            {
+                onclick: null,
+                top_speed: 65,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 12,
+        name: "1963 Rolls-Royce Silver Cloud III",
+        type: "Luxury",
+        price: 699999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 70,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 13,
+        name: "1959 BMW 507 Roadster",
+        type: "Luxury",
+        price: 1502999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 60,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 14,
+        name: "2010 Audi S5 V8",
+        type: "Luxury",
+        price: 364999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 15,
+        name: "2015 Ferrari FF",
+        type: "Luxury",
+        price: 957999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 85,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 16,
+        name: "Mercedes-Benz SL65 AMG",
+        type: "Luxury",
+        price: 495999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 60,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 17,
+        name: "2019 Chevrole Corvette ZR1",
+        type: "Luxury",
+        price: 599999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 115,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 18,
+        name: "2011 Mercedes-Benz SLS AMG",
+        type: "Luxury",
+        price: 495999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 100,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 19,
+        name: "2015 Tesla Model X",
+        type: "Luxury",
+        price: 600000,
+        data: [
+            {
+                onclick: null,
+                top_speed: 100,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "electric",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 20,
+        name: "2012 Tesla Model S",
+        type: "Luxury",
+        price: 289999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 85,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "electric",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 21,
+        name: "2011 Mercedes-Benz E63 AMG",
+        type: "Luxury",
+        price: 135999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 90,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 22,
+        name: "2017 Audi S5",
+        type: "Luxury",
+        price: 404999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 23,
+        name: "2013 Audi RS4 Quattro",
+        type: "Luxury",
+        price: 384999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 90,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 24,
+        name: "1985 Cadillac Fleetwood Brougham d'Elegance",
+        type: "Luxury",
+        price: 45000,
+        data: [
+            {
+                onclick: null,
+                top_speed: 90,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 25,
+        name: "2011 Mercedes McLaren SLR",
+        type: "Luxury",
+        price: 785999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 105,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 26,
+        name: "2010 Audi S4 V6T",
+        type: "Luxury",
+        price: 191999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 27,
+        name: "1963 Mercedes-Benz 300 SEL",
+        type: "Luxury",
+        price: 155999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 70,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: false,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 28,
+        name: "2010 Mercedes-Benz C32 AMG",
+        type: "Luxury",
+        price: 199999,
+        data: [
+            {
+                onclick: null,
+                top_speed: 80,
+                zero_plus: 0,
+                upgrades: ["unknown"],
+                skins: 0,
+                fuel_type: "gas",
+                exclusive: [
+                    {
+                        exclusive_vehicle: true,
+                        exclusive_vehicle_price: 0
+                    }
+                ]
+            }
+        ]
+    },
+]
+
+var categories = ["Compact","Luxury","Miscellaneous","Muscle","Rally","SUV","Sedan","Sports","Super","Truck"]
+
+for(a = 0; a < categories.length; a++){
+    var main = document.getElementById("list");
+    var content = document.createElement("div");
+    content.className = "categories";
+    content.innerHTML = `
+    <h1>${categories[a]}</h1>
+    <ul id="${categories[a]}"><ul>
+    `;
+    main.appendChild(content);
+}
+
+for(b = 0; b < vehicles.length; b++){
+    var exclusiveCar = vehicles[b].data[0].exclusive[0];
+    var category = document.getElementById(`${vehicles[b].type}`);
+    var vehicle = document.createElement("li");
+    vehicle.setAttribute("onclick",`vehicleData(${vehicles[b].id})`);
+    vehicle.id = vehicles[b].id;
+    vehicle.className = `${ exclusiveCar.exclusive_vehicle ? 'vehicle exclusive' : 'vehicle'}`
+    vehicle.innerHTML = `<a style="text-decoration: none; color: inherit;">${vehicles[b].name}</a>`
+    category.appendChild(vehicle);
+}
+
+function vehicleData(id) {
+    var vehinfo = document.getElementById("vehinfo");
+    var exclusiveCar = vehicles[id].data[0].exclusive[0];
+    let n = exclusiveCar.exclusive_vehicle ? exclusiveCar.exclusive_vehicle_price : vehicles[id].price;
+    let str = n.toLocaleString("en-US");
+
+    vehinfo.innerHTML = `<b>${vehicles[id].name}</b><hr><b>Top Speed:</b> ${vehicles[id].data[0].top_speed}<br><b>Fuel Type:</b> ${vehicles[id].data[0].fuel_type}<br><b>Price:</b> $${str}`;
+}
